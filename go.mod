93fa50ce612917b7e7130a2b3c7ea30b4460c0ef
indrariksa <indrariksa@gmail.com>
2023-10-26 15:10:39 +0700

    Fix(go.mod): update pacakge di go.mod

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/PMBULBI/pendaftaran
 go 1.21.0
 
 require (
-	github.com/PMBULBI/types v0.0.5
+	github.com/PMBULBI/types v0.0.6
 	github.com/golang-module/carbon/v2 v2.2.11
+	github.com/golang-module/dongle v0.2.8
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
 )
@@ -12,7 +13,6 @@ require (
 require (
 	github.com/emmansun/gmsm v0.15.5 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/golang-module/dongle v0.2.8 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
--- pendaftaran_test.go
+++ pendaftaran_test.go
@@ -2,6 +2,7 @@ package pendaftaran
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	pmbulbi "github.com/PMBULBI/types/schemas"
 	"github.com/golang-module/carbon/v2"
@@ -17,14 +18,23 @@ func TestPmbdb_TableMigrator(t *testing.T) {
 func TestInsertDataPendaftar(t *testing.T) {
 	conn := CreateMariaGormConnection("MARIA")
 	data := pmbulbi.Pendaftaran{
-		NamaMhs:         "Test",
-		AsalSekolah:     "Bandung High School",
-		EmailMhs:        "Testing@gmail.com",
-		HpMhs:           "62851666465",
-		ProvinsiSekolah: "Jawa barat",
-		KotaSekolah:     "Bandung",
-		UsernameAdmin:   "rofi",
-		TglDaftarMhs:    carbon.Now(),
+		NamaMhs:     "Test",
+		AsalSekolah: "Bandung High School",
+		EmailMhs:    "Testing@gmail.com",
+		HpMhs:       "62851666465",
+		ProvinsiSekolah: sql.NullString{
+			String: "Jawa Barat",
+			Valid:  true,
+		},
+		KotaSekolah: sql.NullString{
+			String: "Bandung",
+			Valid:  true,
+		},
+		UsernameAdmin: sql.NullString{
+			String: "gaben",
+			Valid:  true,
+		},
+		TglDaftarMhs: carbon.Now(),
 	}
 	ins := InsertDataPendaftar(conn, context.Background(), data)
 	fmt.Println(ins)
@@ -42,14 +52,23 @@ func TestEncrypt(t *testing.T) {
 
 func TestPendaftaran(t *testing.T) {
 	insdata := pmbulbi.Pendaftaran{
-		NamaMhs:         "Testeeeee",
-		AsalSekolah:     "Bandung High School",
-		EmailMhs:        "Testing@gmail.com",
-		HpMhs:           "62851666465",
-		ProvinsiSekolah: "Jawa barat",
-		KotaSekolah:     "Bandung",
-		UsernameAdmin:   "rofi",
-		TglDaftarMhs:    carbon.Now(),
+		NamaMhs:     "Test",
+		AsalSekolah: "Bandung High School",
+		EmailMhs:    "Testing@gmail.com",
+		HpMhs:       "62851666465",
+		ProvinsiSekolah: sql.NullString{
+			String: "Jawa Barat",
+			Valid:  true,
+		},
+		KotaSekolah: sql.NullString{
+			String: "Bandung",
+			Valid:  true,
+		},
+		UsernameAdmin: sql.NullString{
+			String: "",
+			Valid:  false,
+		},
+		TglDaftarMhs: carbon.Now(),
 	}
 	data := Pendaftaran(context.Background(), "MARIA", "rofiganteng", insdata)
 	fmt.Println(data)
