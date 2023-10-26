654226793759ff9dc563848c295776a650dc1586
rofinafiin <rofinafiisr@gmail.com>
2023-10-26 11:33:47 +0700

    Feat(InsertdataPendaftar): Fungsi untuk insert data pendaftar ke database

--- function.go
+++ function.go
@@ -1,9 +1,23 @@
 package pendaftaran
 
+import (
+	"context"
+	pmbulbi "github.com/PMBULBI/types/schemas"
+	"gorm.io/gorm"
+)
+
 func TableMigrator(maria string, model interface{}) (res string, err error) {
 	err = CreateMariaGormConnection(maria).AutoMigrate(&model)
 	if err != nil {
 		return "Gagal Migrate", err
 	}
+	return
+}
+
+func InsertDataPendaftar(conn *gorm.DB, ctx context.Context, val pmbulbi.Pendaftaran) (err error) {
+	err = conn.
+		WithContext(ctx).
+		Create(&val).
+		Error
 	return
 }
