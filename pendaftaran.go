1d948b22cbbdef5bffb1050e48e30f69b31c5550
indrariksa <indrariksa@gmail.com>
2023-10-26 15:51:27 +0700

    Fix(pendaftaran): perbaikan Pendaftaran

--- pendaftaran.go
+++ pendaftaran.go
@@ -2,6 +2,7 @@ package pendaftaran
 
 import (
 	"context"
+	"database/sql"
 	pmbulbi "github.com/PMBULBI/types/schemas"
 	"github.com/golang-module/carbon/v2"
 )
@@ -11,16 +12,23 @@ func Pendaftaran(ctx context.Context, Mariaenv, secret string, val pmbulbi.Penda
 
 	mypass := GenerateRandomPassword(10)
 	passwordencrypted := Encrypt(mypass, secret)
+
 	data := pmbulbi.Pendaftaran{
-		NamaMhs:         val.NamaMhs,
-		AsalSekolah:     val.AsalSekolah,
-		EmailMhs:        val.EmailMhs,
-		HpMhs:           val.HpMhs,
-		ProvinsiSekolah: val.ProvinsiSekolah,
-		KotaSekolah:     val.KotaSekolah,
-		Password:        passwordencrypted,
-		UsernameAdmin:   val.UsernameAdmin,
-		TglDaftarMhs:    carbon.Now(),
+		NamaMhs:     val.NamaMhs,
+		AsalSekolah: val.AsalSekolah,
+		EmailMhs:    val.EmailMhs,
+		HpMhs:       val.HpMhs,
+		ProvinsiSekolah: sql.NullString{
+			String: val.ProvinsiSekolah.String,
+			Valid:  val.ProvinsiSekolah.String != ""},
+		KotaSekolah: sql.NullString{
+			String: val.KotaSekolah.String,
+			Valid:  val.KotaSekolah.String != ""},
+		Password: passwordencrypted,
+		UsernameAdmin: sql.NullString{
+			String: val.UsernameAdmin.String,
+			Valid:  val.UsernameAdmin.String != ""},
+		TglDaftarMhs: carbon.Now(),
 	}
 	err = InsertDataPendaftar(conn, ctx, data)
 	if err != nil {
