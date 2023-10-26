78d7a59b9cde14e9c92a36465d0b48d7e46a2c17
rofinafiin <rofinafiisr@gmail.com>
2023-10-26 11:04:08 +0700

    Feat(TableMigrator): Fungsi untuk migrate table langsung dari struct

---
+++ function.go
@@ -0,0 +1,9 @@
+package pendaftaran
+
+func TableMigrator(maria string, model interface{}) (res string, err error) {
+	err = CreateMariaGormConnection(maria).AutoMigrate(&model)
+	if err != nil {
+		return "Gagal Migrate", err
+	}
+	return
+}
