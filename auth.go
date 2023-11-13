9ae5cdfd0413d4d0e67693f91802c012adc58087
JPratama7 <josepratama080@gmail.com>
2023-11-13 15:18:53 +0700

    feat(repository): new method untuk get data berdasarkan email/phonenum dan pass

---
+++ auth.go
@@ -0,0 +1,29 @@
+package pendaftaran
+
+import (
+	"context"
+	"github.com/PMBULBI/types/schemas"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	db *gorm.DB
+}
+
+func (r *Repository) GetByEmailPass(ctx context.Context, email, password string) (data schemas.Pendaftaran, err error) {
+	err = r.db.WithContext(ctx).
+		Where("email_mhs = ? AND password = ?", email, password).
+		First(&data).
+		Error
+	return
+
+}
+
+func (r *Repository) GetByPhoneNumPass(ctx context.Context, email, password string) (data schemas.Pendaftaran, err error) {
+	err = r.db.WithContext(ctx).
+		Where("hp_mhs = ? AND password = ?", email, password).
+		First(&data).
+		Error
+	return
+
+}
