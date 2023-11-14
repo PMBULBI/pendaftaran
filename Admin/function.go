ca7e4a3dc29b14217a43973dbf978c0e479ac8b6
indrariksa <indrariksa@gmail.com>
2023-11-14 15:49:53 +0700

    fix(function): pindah fungsi ke directory Admin

--- Admin/function.go
+++ Admin/function.go
@@ -2,24 +2,58 @@ package Admin
 
 import (
 	"context"
+	"github.com/PMBULBI/pendaftaran"
 	pmbulbi "github.com/PMBULBI/types/schemas"
-	"gorm.io/gorm"
+	"github.com/golang-module/carbon/v2"
 )
 
-func GetAllPendaftar(conn *gorm.DB, ctx context.Context) (val []pmbulbi.Pendaftaran, err error) {
-	err = conn.
-		WithContext(ctx).
-		Order("id DESC").
-		Find(&val).
-		Error
-	return
+func GetAllPendaftaran(ctx context.Context, Mariaenv string) (dataProvinsi []pmbulbi.ResponsePendaftaran, err error) {
+	conn := pendaftaran.CreateMariaGormConnection(Mariaenv)
+
+	val, err := GetAllPendaftar(conn, ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range val {
+		dataProvinsi = append(dataProvinsi, pmbulbi.ResponsePendaftaran{
+			ID:              v.ID,
+			NamaMhs:         v.NamaMhs,
+			AsalSekolah:     v.AsalSekolah,
+			EmailMhs:        v.EmailMhs,
+			HpMhs:           v.HpMhs,
+			ProvinsiSekolah: v.ProvinsiSekolah.String,
+			KotaSekolah:     v.KotaSekolah.String,
+			Password:        v.Password,
+			StatusMhs:       v.StatusMhs,
+			UsernameAdmin:   v.UsernameAdmin.String,
+			TglDaftarMhs:    carbon.DateTime{v.TglDaftarMhs},
+		})
+	}
+
+	return dataProvinsi, nil
 }
 
-func GetSatuPendaftar(conn *gorm.DB, ctx context.Context, id string) (dest pmbulbi.Pendaftaran, err error) {
-	err = conn.
-		WithContext(ctx).
-		Where("id = ? ", id).
-		First(&dest).
-		Error
-	return
+func GetOnePendaftar(ctx context.Context, Mariaenv, id string) (data pmbulbi.ResponsePendaftaran, err error) {
+	conn := pendaftaran.CreateMariaGormConnection(Mariaenv)
+
+	user, err := GetSatuPendaftar(conn, ctx, id)
+	if err != nil {
+		return pmbulbi.ResponsePendaftaran{}, err
+	}
+
+	data.ID = user.ID
+	data.NamaMhs = user.NamaMhs
+	data.AsalSekolah = user.AsalSekolah
+	data.EmailMhs = user.EmailMhs
+	data.HpMhs = user.HpMhs
+	data.ProvinsiSekolah = user.ProvinsiSekolah.String
+	data.KotaSekolah = user.KotaSekolah.String
+	data.Password = user.Password
+	data.StatusMhs = user.StatusMhs
+	data.UsernameAdmin = user.UsernameAdmin.String
+	data.TglDaftarMhs = carbon.DateTime{Carbon: user.TglDaftarMhs}
+
+	return data, nil
 }
--- pendaftaran.go
+++ pendaftaran.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"github.com/PMBULBI/pendaftaran/Admin"
 	pmbulbi "github.com/PMBULBI/types/schemas"
 	"github.com/golang-module/carbon/v2"
 )
@@ -44,34 +43,6 @@ func Pendaftaran(ctx context.Context, Mariaenv, secret string, val pmbulbi.Penda
 	return
 }
 
-func GetAllPendaftaran(ctx context.Context, Mariaenv string) (dataProvinsi []pmbulbi.ResponsePendaftaran, err error) {
-	conn := CreateMariaGormConnection(Mariaenv)
-
-	val, err := Admin.GetAllPendaftar(conn, ctx)
-
-	if err != nil {
-		return nil, err
-	}
-
-	for _, v := range val {
-		dataProvinsi = append(dataProvinsi, pmbulbi.ResponsePendaftaran{
-			ID:              v.ID,
-			NamaMhs:         v.NamaMhs,
-			AsalSekolah:     v.AsalSekolah,
-			EmailMhs:        v.EmailMhs,
-			HpMhs:           v.HpMhs,
-			ProvinsiSekolah: v.ProvinsiSekolah.String,
-			KotaSekolah:     v.KotaSekolah.String,
-			Password:        v.Password,
-			StatusMhs:       v.StatusMhs,
-			UsernameAdmin:   v.UsernameAdmin.String,
-			TglDaftarMhs:    carbon.DateTime{v.TglDaftarMhs},
-		})
-	}
-
-	return dataProvinsi, nil
-}
-
 func GetAllProvinsi(ctx context.Context, Mariaenv string) (dataProvinsi []pmbulbi.ProvinsiResponse, err error) {
 	conn := CreateMariaGormConnection(Mariaenv)
 
@@ -109,29 +80,6 @@ func GetAllKota(ctx context.Context, Mariaenv string) (dataKota []pmbulbi.KotaRe
 	}
 
 	return dataKota, nil
-}
-
-func GetOnePendaftar(ctx context.Context, Mariaenv, id string) (data pmbulbi.ResponsePendaftaran, err error) {
-	conn := CreateMariaGormConnection(Mariaenv)
-
-	user, err := Admin.GetSatuPendaftar(conn, ctx, id)
-	if err != nil {
-		return pmbulbi.ResponsePendaftaran{}, err
-	}
-
-	data.ID = user.ID
-	data.NamaMhs = user.NamaMhs
-	data.AsalSekolah = user.AsalSekolah
-	data.EmailMhs = user.EmailMhs
-	data.HpMhs = user.HpMhs
-	data.ProvinsiSekolah = user.ProvinsiSekolah.String
-	data.KotaSekolah = user.KotaSekolah.String
-	data.Password = user.Password
-	data.StatusMhs = user.StatusMhs
-	data.UsernameAdmin = user.UsernameAdmin.String
-	data.TglDaftarMhs = carbon.DateTime{Carbon: user.TglDaftarMhs}
-
-	return data, nil
 }
 
 func GetDaftarSekolahlimit5(ctx context.Context, Mariaenv string, limit int, schoolname string) (data []pmbulbi.DaftarSekolah, err error) {
