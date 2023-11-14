4c910bca9932c8342bf537e426b2fe3f53bbf790
indrariksa <indrariksa@gmail.com>
2023-11-14 16:04:40 +0700

    Feat(GetAllLevelAdm): function get semua lvl admin

--- admin/function.go
+++ admin/function.go
@@ -57,3 +57,22 @@ func GetOnePendaftar(ctx context.Context, Mariaenv, id string) (data pmbulbi.Res
 
 	return data, nil
 }
+
+func GetAllLevelAdm(ctx context.Context, Mariaenv string) (dataLvlAdm []pmbulbi.AdminLevel, err error) {
+	conn := pendaftaran.CreateMariaGormConnection(Mariaenv)
+
+	val, err := GetAllLevelAdmin(conn, ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range val {
+		dataLvlAdm = append(dataLvlAdm, pmbulbi.AdminLevel{
+			IDLevel:   v.IDLevel,
+			NamaLevel: v.NamaLevel,
+		})
+	}
+
+	return dataLvlAdm, nil
+}
