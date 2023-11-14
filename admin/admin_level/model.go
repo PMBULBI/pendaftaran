806095a7f55451496b852db389d4d7d8747120b9
indrariksa <indrariksa@gmail.com>
2023-11-14 16:18:11 +0700

    fix(model): pindah ke directory admin_level

--- admin/admin_level/model.go
+++ admin/admin_level/model.go
@@ -6,24 +6,6 @@ import (
 	"gorm.io/gorm"
 )
 
-func GetAllPendaftar(conn *gorm.DB, ctx context.Context) (val []pmbulbi.Pendaftaran, err error) {
-	err = conn.
-		WithContext(ctx).
-		Order("id DESC").
-		Find(&val).
-		Error
-	return
-}
-
-func GetSatuPendaftar(conn *gorm.DB, ctx context.Context, id string) (dest pmbulbi.Pendaftaran, err error) {
-	err = conn.
-		WithContext(ctx).
-		Where("id = ? ", id).
-		First(&dest).
-		Error
-	return
-}
-
 func GetAllLevelAdmin(conn *gorm.DB, ctx context.Context) (val []pmbulbi.AdminLevel, err error) {
 	err = conn.
 		WithContext(ctx).
