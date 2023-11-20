513c3a40bf576d950f67d5e9e8bf019e63bef7eb
indrariksa <indrariksa@gmail.com>
2023-11-20 15:27:12 +0700

    feat(InsertBiodataProdi): function insert biodata program studi

--- mahasiswa/biodata/function.go
+++ mahasiswa/biodata/function.go
@@ -2,6 +2,7 @@ package biodata
 
 import (
 	"context"
+	"database/sql"
 	pmbulbi "github.com/PMBULBI/types/schemas"
 	"gorm.io/gorm"
 )
@@ -15,6 +16,25 @@ func InsertBiodataJalur(ctx context.Context, Mariaconn *gorm.DB, val pmbulbi.Bio
 		KodeRef:    val.KodeRef,
 	}
 	err = InsertBioJalur(Mariaconn, ctx, data)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+func InsertBiodataProdi(ctx context.Context, Mariaconn *gorm.DB, val pmbulbi.BiodataProdi) (err error) {
+
+	data := pmbulbi.BiodataProdi{
+		IdHash:     val.IdHash,
+		IDFakultas: val.IDFakultas,
+		Prodi1:     val.Prodi1,
+		Prodi2:     val.Prodi2,
+		Konsentrasi: sql.NullInt16{
+			Int16: val.Konsentrasi.Int16,
+			Valid: val.Konsentrasi.Int16 != 0,
+		},
+	}
+	err = InsertBioProdi(Mariaconn, ctx, data)
 	if err != nil {
 		return err
 	}
