dcc8ad01cc366769e5872ee702922167aa9a9eff
rofinafiin <rofinafiisr@gmail.com>
2023-11-21 13:52:15 +0700

    feat(InsertBiodiri): function insert data diri

--- mahasiswa/biodata/function.go
+++ mahasiswa/biodata/function.go
@@ -2,20 +2,12 @@ package biodata
 
 import (
 	"context"
-	"database/sql"
 	pmbulbi "github.com/PMBULBI/types/schemas"
 	"gorm.io/gorm"
 )
 
 func InsertBiodataJalur(ctx context.Context, Mariaconn *gorm.DB, val pmbulbi.BiodataJalur) (err error) {
-
-	data := pmbulbi.BiodataJalur{
-		IdHash:     val.IdHash,
-		IDJalur:    val.IDJalur,
-		TahunLulus: val.TahunLulus,
-		KodeRef:    val.KodeRef,
-	}
-	err = InsertBioJalur(Mariaconn, ctx, data)
+	err = InsertBioJalur(Mariaconn, ctx, val)
 	if err != nil {
 		return err
 	}
@@ -23,18 +15,15 @@ func InsertBiodataJalur(ctx context.Context, Mariaconn *gorm.DB, val pmbulbi.Bio
 }
 
 func InsertBiodataProdi(ctx context.Context, Mariaconn *gorm.DB, val pmbulbi.BiodataProdi) (err error) {
-
-	data := pmbulbi.BiodataProdi{
-		IdHash:     val.IdHash,
-		IDFakultas: val.IDFakultas,
-		Prodi1:     val.Prodi1,
-		Prodi2:     val.Prodi2,
-		Konsentrasi: sql.NullInt16{
-			Int16: val.Konsentrasi.Int16,
-			Valid: val.Konsentrasi.Int16 != 0,
-		},
+	err = InsertBioProdi(Mariaconn, ctx, val)
+	if err != nil {
+		return err
 	}
-	err = InsertBioProdi(Mariaconn, ctx, data)
+	return
+}
+
+func InsertBiodataDiri(ctx context.Context, Mariaconn *gorm.DB, val pmbulbi.BiodataDataDiri) (err error) {
+	err = InsertBioDiri(Mariaconn, ctx, val)
 	if err != nil {
 		return err
 	}
