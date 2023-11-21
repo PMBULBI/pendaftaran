5c74890fa957a68f31f9ee8deb3986ab9f0e8b7f
indrariksa <indrariksa@gmail.com>
2023-11-21 10:48:26 +0700

    feat(UpdateJalur): function update jalur

--- jalur/function.go
+++ jalur/function.go
@@ -52,3 +52,20 @@ func InsertJalur(ctx context.Context, Mariaconn *gorm.DB, val pmbulbi.JalurPenda
 	}
 	return
 }
+
+func UpdateJalur(ctx context.Context, Mariaconn *gorm.DB, id string, val pmbulbi.JalurPendaftaran) (err error) {
+
+	data := pmbulbi.JalurPendaftaran{
+		IDJalur:         val.IDJalur,
+		Jalur:           val.Jalur,
+		NamaJalur:       val.NamaJalur,
+		KeteranganJalur: val.KeteranganJalur,
+		Status:          val.Status,
+	}
+
+	err = UpdJalur(Mariaconn, ctx, id, data)
+	if err != nil {
+		return err
+	}
+	return
+}
