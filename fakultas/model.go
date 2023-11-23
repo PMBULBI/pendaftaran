48c1ebb98977e1ba2b4df1f2465cd95b7cf417fc
indrariksa <indrariksa@gmail.com>
2023-11-23 15:22:39 +0700

    refactor(fakultas): refactor per module

--- fakultas/model.go
+++ fakultas/model.go
@@ -3,19 +3,18 @@ package fakultas
 import (
 	"context"
 	pmbulbi "github.com/PMBULBI/types/schemas"
-	"gorm.io/gorm"
 )
 
-func GetMAllFakultas(conn *gorm.DB, ctx context.Context) (val []pmbulbi.Fakultas, err error) {
-	err = conn.
+func (r *Repository) GetMAllFakultas(ctx context.Context) (val []pmbulbi.Fakultas, err error) {
+	err = r.db.
 		WithContext(ctx).
 		Find(&val).
 		Error
 	return
 }
 
-func GetMOneFakultas(conn *gorm.DB, ctx context.Context, id string) (val pmbulbi.Fakultas, err error) {
-	err = conn.
+func (r *Repository) GetMOneFakultas(ctx context.Context, id string) (val pmbulbi.Fakultas, err error) {
+	err = r.db.
 		WithContext(ctx).
 		Where("id_fakultas = ? ", id).
 		First(&val).
@@ -23,16 +22,16 @@ func GetMOneFakultas(conn *gorm.DB, ctx context.Context, id string) (val pmbulbi
 	return
 }
 
-func InsFakultas(conn *gorm.DB, ctx context.Context, val pmbulbi.Fakultas) (err error) {
-	err = conn.
+func (r *Repository) InsFakultas(ctx context.Context, val pmbulbi.Fakultas) (err error) {
+	err = r.db.
 		WithContext(ctx).
 		Create(&val).
 		Error
 	return
 }
 
-func UpdFakultas(conn *gorm.DB, ctx context.Context, id string, val pmbulbi.Fakultas) (err error) {
-	err = conn.
+func (r *Repository) UpdFakultas(ctx context.Context, id string, val pmbulbi.Fakultas) (err error) {
+	err = r.db.
 		WithContext(ctx).
 		Where("id_fakultas = ?", id).
 		Updates(&val).
@@ -40,8 +39,8 @@ func UpdFakultas(conn *gorm.DB, ctx context.Context, id string, val pmbulbi.Faku
 	return
 }
 
-func DelFakultas(conn *gorm.DB, ctx context.Context, id string) (err error) {
-	err = conn.
+func (r *Repository) DelFakultas(ctx context.Context, id string) (err error) {
+	err = r.db.
 		WithContext(ctx).
 		Where("id_fakultas = ?", id).
 		Delete(&pmbulbi.Fakultas{}).
