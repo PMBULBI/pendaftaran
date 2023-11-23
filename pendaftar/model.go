694487f7c2a051a9f49ce2e64e4532f06d87b229
JPratama7 <josepratama080@gmail.com>
2023-11-23 15:05:04 +0700

    refactor(pendaftar): refactor per module

---
+++ pendaftar/model.go
@@ -0,0 +1,24 @@
+package pendaftar
+
+import (
+	"context"
+	pmbulbi "github.com/PMBULBI/types/schemas"
+)
+
+func (r *Repository) Fetch(ctx context.Context) (val []pmbulbi.Pendaftaran, err error) {
+	err = r.db.
+		WithContext(ctx).
+		Order("id DESC").
+		Find(&val).
+		Error
+	return
+}
+
+func (r *Repository) GetById(ctx context.Context, id string) (dest pmbulbi.Pendaftaran, err error) {
+	err = r.db.
+		WithContext(ctx).
+		Where("id = ? ", id).
+		First(&dest).
+		Error
+	return
+}
