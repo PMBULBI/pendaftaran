64a5cec4cf7493b6edee3eb6a47716331940aed1
indrariksa <indrariksa@gmail.com>
2023-11-24 15:03:12 +0700

    refactor(kota): refactor per module (GetKotaByIdProvinsiNamaKota)

--- kota/model.go
+++ kota/model.go
@@ -12,3 +12,12 @@ func (r *Repository) Fetch(ctx context.Context) (val []pmbulbi.WilayahKota, err 
 		Error
 	return
 }
+
+func (r *Repository) GetKotaByIdProvinsiNamaKota(ctx context.Context, id_prov string, kota string) (dest []pmbulbi.WilayahKota, err error) {
+	err = r.db.
+		WithContext(ctx).
+		Where("id_provinsi LIKE ? AND nama_kota LIKE ?", "%"+id_prov+"%", "%"+kota+"%").
+		Find(&dest).
+		Error
+	return
+}
