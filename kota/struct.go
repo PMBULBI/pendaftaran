b232caae6c8a332a73780c573c1477ed59b6524d
indrariksa <indrariksa@gmail.com>
2023-11-24 14:57:44 +0700

    refactor(kota): refactor per module tambah repo

---
+++ kota/struct.go
@@ -0,0 +1,16 @@
+package kota
+
+import (
+	pmbulbi "github.com/PMBULBI/types/schemas"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		db: db.Session(&gorm.Session{}).Model(&pmbulbi.WilayahKota{}),
+	}
+}
