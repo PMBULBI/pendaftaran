251bfa3d58ca4dc33461f33be6235b5030f634a4
indrariksa <indrariksa@gmail.com>
2023-11-24 15:11:15 +0700

    refactor(sekolah): refactor per module (GetSekolahLimits)

---
+++ daftar_sekolah/model.go
@@ -0,0 +1,16 @@
+package daftar_sekolah
+
+import (
+	"context"
+	pmbulbi "github.com/PMBULBI/types/schemas"
+)
+
+func (r *Repository) GetSekolahLimits(ctx context.Context, lim int, name string) (dest []pmbulbi.DaftarSekolah, err error) {
+	err = r.db.
+		WithContext(ctx).
+		Limit(lim).
+		Where("nama_sekolah LIKE ?", "%"+name+"%").
+		Find(&dest).
+		Error
+	return
+}
