a0c1638a77d707685e78c62cefde7cdc04d871ef
JPratama7 <josepratama080@gmail.com>
2023-11-29 14:06:20 +0700

    feat(pendaftar): penambahan method untuk authentication

--- pendaftar/method.go
+++ pendaftar/method.go
@@ -30,3 +30,32 @@ func (r *Repository) Insert(ctx context.Context, val pmbulbi.Pendaftaran) (err e
 		Error
 	return
 }
+
+func (r *Repository) GetByEmailPass(ctx context.Context, email, password string) (data pmbulbi.Pendaftaran, err error) {
+	err = r.db.WithContext(ctx).
+		Where("email_mhs = ? AND password = ?", email, password).
+		First(&data).
+		Error
+	return
+}
+func (r *Repository) GetByPhoneNumPass(ctx context.Context, email, password string) (data pmbulbi.Pendaftaran, err error) {
+	err = r.db.WithContext(ctx).
+		Where("hp_mhs = ? AND password = ?", email, password).
+		First(&data).
+		Error
+	return
+}
+
+func (r *Repository) CheckUserExists(ctx context.Context, email, phoneNum string) (res bool) {
+	data := pmbulbi.Pendaftaran{}
+	err := r.db.WithContext(ctx).
+		Where("email_mhs = ? AND hp_mhs = ?", email, phoneNum).
+		First(&data).
+		Error
+	if err != nil {
+		return
+	}
+
+	res = data != (pmbulbi.Pendaftaran{})
+	return
+}
