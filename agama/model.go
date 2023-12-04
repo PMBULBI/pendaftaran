05177be1ba2bb5e328cca2d975a605e803d4bd62
indrariksa <indrariksa@gmail.com>
2023-12-04 14:50:05 +0700

    feat(agama): model Update

--- agama/model.go
+++ agama/model.go
@@ -29,3 +29,12 @@ func (r *Repository) Insert(ctx context.Context, val pmbulbi.Agama) (err error) 
 		Error
 	return
 }
+
+func (r *Repository) Update(ctx context.Context, id string, val pmbulbi.Agama) (err error) {
+	err = r.db.
+		WithContext(ctx).
+		Where("id = ?", id).
+		Updates(&val).
+		Error
+	return
+}
