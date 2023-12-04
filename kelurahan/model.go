0ba4e7f74c91300975fedfbb85cc8d2b456c8ad4
indrariksa <indrariksa@gmail.com>
2023-12-04 14:56:26 +0700

    feat(kelurahan): model Fetch

---
+++ kelurahan/model.go
@@ -0,0 +1,14 @@
+package kelurahan
+
+import (
+	"context"
+	pmbulbi "github.com/PMBULBI/types/schemas"
+)
+
+func (r *Repository) Fetch(ctx context.Context) (val []pmbulbi.WilayahKelurahan, err error) {
+	err = r.db.
+		WithContext(ctx).
+		Find(&val).
+		Error
+	return
+}
