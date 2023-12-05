6c42253779d6587c09c6958c742b3e6416329464
indrariksa <indrariksa@gmail.com>
2023-12-05 16:39:27 +0700

    feat(pekerjaan): repo struct

---
+++ pekerjaan/struct.go
@@ -0,0 +1,16 @@
+package pekerjaan
+
+import (
+	pmbulbi "github.com/PMBULBI/types/schemas"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		db: db.Session(&gorm.Session{}).Model(&pmbulbi.Pekerjaan{}),
+	}
+}
