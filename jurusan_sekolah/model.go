aea73a6b2adb6d09f0cc77778d0034d8fb6b95d7
indrariksa <indrariksa@gmail.com>
2023-12-05 16:51:49 +0700

    feat(jurusan_sekolah): model GetJurusanByIdJnsSekolah

--- jurusan_sekolah/model.go
+++ jurusan_sekolah/model.go
@@ -12,3 +12,12 @@ func (r *Repository) Fetch(ctx context.Context) (val []pmbulbi.JurusanSekolah, e
 		Error
 	return
 }
+
+func (r *Repository) GetJurusanByIdJnsSekolah(ctx context.Context, id_jenis_sekolah string) (dest []pmbulbi.JurusanSekolah, err error) {
+	err = r.db.
+		WithContext(ctx).
+		Where("id_jenis_sekolah LIKE ?", "%"+id_jenis_sekolah+"%").
+		Find(&dest).
+		Error
+	return
+}
