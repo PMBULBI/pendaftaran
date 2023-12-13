8f3d74ad5d507da6ccbf36ca56df47344f5af305
indrariksa <indrariksa@gmail.com>
2023-12-13 10:50:10 +0700

    fix(prodi): ubah order by desc

--- tahun_lulusan/model.go
+++ tahun_lulusan/model.go
@@ -8,6 +8,7 @@ import (
 func (r *Repository) Fetch(ctx context.Context) (val []pmbulbi.TahunLulusan, err error) {
 	err = r.db.
 		WithContext(ctx).
+		Order("tahun DESC").
 		Find(&val).
 		Error
 	return
