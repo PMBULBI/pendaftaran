c51d4b3ea1699db0da1c8be9159a66356e5b3c87
indrariksa <indrariksa@gmail.com>
2023-12-14 10:21:05 +0700

    feat(jalur_berkas): model GetBerkasByJalur

--- jalur_berkas/model.go
+++ jalur_berkas/model.go
@@ -12,3 +12,15 @@ func (r *Repository) Fetch(ctx context.Context) (val []pmbulbi.JalurBerkas, err 
 		Error
 	return
 }
+
+func (r *Repository) GetBerkasByJalur(ctx context.Context, id_jalur int) (dest []pmbulbi.JalurBerkasJoin, err error) {
+	err = r.db.
+		WithContext(ctx).
+		Table("jalur_berkas jaber").
+		Joins("JOIN jalur_pendaftaran japen ON japen.id_jalur = jaber.id_jalur").
+		Where("jaber.id_jalur = ?", id_jalur).
+		Select("jaber.*, japen.*").
+		Find(&dest).
+		Error
+	return
+}
