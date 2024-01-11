afd8283b404a2350ee456adbde36113cf644bc3c
Indra Riksa Herlambang <indrariksa@gmail.com>
2024-01-11 14:56:30 +0700

    feat(konsentrasi): model insert

--- konsentrasi/model.go
+++ konsentrasi/model.go
@@ -21,3 +21,11 @@ func (r *Repository) GetById(ctx context.Context, id string) (val pmbulbi.Konsen
 		Error
 	return
 }
+
+func (r *Repository) Insert(ctx context.Context, val pmbulbi.Konsentrasi) (err error) {
+	err = r.db.
+		WithContext(ctx).
+		Create(&val).
+		Error
+	return
+}
